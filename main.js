d333c0f36ea346a6c9042572c7695e6bcb2567e6
Fahad Guled <fahad.guled@yahoo.com>
2022-01-28 13:56:30 -0800

    got the game to work. Now i only need to put a limit on the number of times the user can guess.

---
+++ main.js
@@ -0,0 +1,17 @@
+const button = document.getElementById('sumbit');
+const yourGuess = document.getElementById('yourguess');
+const text = document.getElementById('userinput');
+
+const number = Math.floor(Math.random()*100);
+
+button.addEventListener('click', () => {
+    let input = document.getElementById('userinput').value;
+
+    if(input == number) {
+        yourGuess.innerHTML = `Congrats! The number ${number} was CORRECT!`
+    } else if(input > number) {
+        yourGuess.innerHTML = `Sorry! ${input} was too high.`
+    } else if(input < number) {
+        yourGuess.innerHTML = `Sorry! ${input} was too low.`
+    }
+})
