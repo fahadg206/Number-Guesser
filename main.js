04c3f87b2b0e3f7c5a67da6444725556ee027fab
Fahad Guled <fahad.guled@yahoo.com>
2022-01-29 13:56:05 -0800

    added a guess counter to keep track of how many times the user has guessed a number.

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const button = document.getElementById('sumbit');
 const yourGuess = document.getElementById('yourguess');
 const text = document.getElementById('userinput');
+const guessCounter = document.getElementById("numofguesses");
+let numGuesses = 0;
 
 const number = Math.floor(Math.random()*100);
+let userGuess = text.value;
+let storeGuesses = [];
+
 
 button.addEventListener('click', () => {
     let input = document.getElementById('userinput').value;
+
+      // Keep track of how many times the user takes a guess
+    numGuesses += 1;
+    guessCounter.innerHTML = "Number of Guesses: " + numGuesses;
 
     if(input == number) {
         yourGuess.innerHTML = `Congrats! The number ${number} was CORRECT!`
@@ -13,9 +22,13 @@ button.addEventListener('click', () => {
         yourGuess.innerHTML = `Sorry! ${input} was too high.`
     } else if(input < number) {
         yourGuess.innerHTML = `Sorry! ${input} was too low.`
-    }
+    };
+
+
+
+
 }
 
-    
-
 )
+
+    
