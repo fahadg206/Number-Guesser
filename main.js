b25a0f8bfa0b4507ca5fe35e928915b452c49e47
Fahad Guled <fahad.guled@yahoo.com>
2022-01-30 22:03:08 -0800

    added an empty array and pushed user guesses to display their guesses. Also limited guesses to 3 and displayed game over signal after 3rd guess to the user abd revealed the correct number to the user.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const button = document.getElementById('sumbit');
-const yourGuess = document.getElementById('yourguess');
+const hint = document.getElementById('hint');
 const text = document.getElementById('userinput');
 const guessCounter = document.getElementById("numofguesses");
-let numGuesses = 0;
+const yourGuesses = document.getElementById('yourguesses');
+let numOfGuesses = 0;
 
 const number = Math.floor(Math.random()*100);
 let userGuess = text.value;
@@ -11,21 +12,28 @@ let storeGuesses = [];
 
 button.addEventListener('click', () => {
     let input = document.getElementById('userinput').value;
-
+    let arrayOfGuesses = storeGuesses.push(input);
       // Keep track of how many times the user takes a guess
-    numGuesses += 1;
-    guessCounter.innerHTML = "Number of Guesses: " + numGuesses;
+    numOfGuesses += 1;
+    guessCounter.innerHTML = "Number of Guesses: " + numOfGuesses;
 
+    //hint logic
     if(input == number) {
-        yourGuess.innerHTML = `Congrats! The number ${number} was CORRECT!`
+        hint.innerHTML = `Congrats! The number ${number} was CORRECT!`;
+        yourGuesses.innerHTML = "Your Guesses:" + " " + storeGuesses;
     } else if(input > number) {
-        yourGuess.innerHTML = `Sorry! ${input} was too high.`
+        hint.innerHTML = `Sorry! ${input} was too high.`;
+        yourGuesses.innerHTML = "Your Guesses:" + " " + storeGuesses;
     } else if(input < number) {
-        yourGuess.innerHTML = `Sorry! ${input} was too low.`
+        hint.innerHTML = `Sorry! ${input} was too low.`;
+        yourGuesses.innerHTML = "Your Guesses:" + " " + storeGuesses;
     };
 
+    if(numOfGuesses === 3) {
+        hint.innerHTML = `Better luck next time! The correct number was ${number}`;
+    }
 
-
+    
 
 }
 
